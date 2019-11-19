5776e1d3e98000ab590e070bbe94c2f68fff00f4
LAUREN CUI <lauren7cui@gmail.com>
2019-11-19 14:59:14 -0800

    Initial commit

---
+++ src/component/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class App extends Component{//classes are interfaces of objects
+    constructor(){
+        super();//obj need defined
+    }
+
+    render(){
+        return (
+            <div>
+                AHA
+            </div>
+        )
+    }
+}
+
+export default App;//share this file to others
